e5eed43556f57df97d9a97cee1bb5bc3c3b660dd
Moriarty221B <munesum@ymail.com>
2019-12-07 19:36:59 +0200

    Updates

---
+++ precache-manifest.fe18b891b5f67c9f90766a8acf35b761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc68f80fc0eeacc4596a4ff1097e704",
+    "url": "/poketimesdeploy/index.html"
+  },
+  {
+    "revision": "9beeec905a0cb4389791",
+    "url": "/poketimesdeploy/static/css/main.ab5063ee.chunk.css"
+  },
+  {
+    "revision": "d0b57397cc79527436c1",
+    "url": "/poketimesdeploy/static/js/2.e847feec.chunk.js"
+  },
+  {
+    "revision": "9beeec905a0cb4389791",
+    "url": "/poketimesdeploy/static/js/main.7174b334.chunk.js"
+  },
+  {
+    "revision": "820cc93a74b82c9f5f84",
+    "url": "/poketimesdeploy/static/js/runtime-main.27a8909f.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/poketimesdeploy/static/media/pokeball.f10bf078.png"
+  }
+]);
